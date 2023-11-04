634408ecdb7cb7192f9413cc99fa785614f33645
Catiflam <gilfrid1@gmail.com>
2023-11-04 10:07:10 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<script>
+import ProjectList from "./components/projects/Projectlist.vue";
+import axios from "axios";
+export default {
+	data() {
+		return {
+			post: [],
+			api: {
+				baseUrl: "http://127.0.0.1:8000/api/",
+			},
+		};
+	},
+	components: { ProjectList },
+
+	methods: {
+		fetchProject(uri = this.api.baseUrl + "project") {
+			axios.get(uri).then((response) => {
+				this.project = response.data.data;
+			});
+		},
+	},
+
+	created() {
+		this.fetchProject();
+	},
+};
+</script>
+
+<template>
+	<h1>Hello World</h1>
+	<hr />
+	<ProjectList />
+</template>
+
+<style lang="scss"></style>
---
+++ src/components/projects/ProjectList.vue
@@ -0,0 +1,28 @@
+<script>
+export default {
+	data() {
+		return {};
+	},
+
+	props: {
+		projects: Array,
+	},
+};
+</script>
+
+<template>
+	<div class="container">
+		<h2>ProjectList</h2>
+		<div class="row row-cols-3 g-4">
+			<div class="col" v-for="i in 6">
+				<div class="card">
+					<div class="card-body">
+						<h4>ProjectTitle</h4>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</template>
+
+<style lang="scss" scoped></style>
